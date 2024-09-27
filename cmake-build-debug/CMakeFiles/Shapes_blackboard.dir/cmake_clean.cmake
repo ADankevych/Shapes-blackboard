6e9a66125dacac3678ac035fba0994564eecad20
adankevych <adankevych@kse.org.ua>
2024-09-28 00:04:56 +0300

    created project

---
+++ cmake-build-debug/CMakeFiles/Shapes_blackboard.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Shapes_blackboard.dir/main.cpp.o"
+  "CMakeFiles/Shapes_blackboard.dir/main.cpp.o.d"
+  "Shapes_blackboard"
+  "Shapes_blackboard.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Shapes_blackboard.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Shapes_blackboard.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for Shapes_blackboard.
+# This may be replaced when dependencies are built.
