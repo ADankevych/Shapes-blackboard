2f9fabac6922bb7e70dc434df3754c776c42603f
adankevych <adankevych@kse.org.ua>
2024-10-03 22:26:33 +0300

    blackboard drawing

--- main.cpp
+++ main.cpp
@@ -1,7 +1,35 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
+const int BOARD_WIDTH = 90;
+const int BOARD_HEIGHT = 30;
+
+struct Board {  // Here I used a part of the code from assignment example
+    vector<vector<char>> grid;
+
+    Board() : grid(BOARD_HEIGHT, vector<char>(BOARD_WIDTH, ' ')) {}
+
+    void print() {
+        for (int i = 0; i < BOARD_WIDTH / 2; i++) {
+            cout << " -";
+        }
+        cout << "\n";
+        for (auto &row: grid) {
+            cout << "|";
+            for (char c: row) {
+                cout << c;
+            }
+            cout << "|\n";
+        }
+        for (int i = 0; i < BOARD_WIDTH / 2; i++) {
+            cout << " -";
+        }
+    }
+};
+
 int main() {
-    cout << "Hello, World!" << endl;
+    Board board;
+    board.print();
     return 0;
 }
